ffe45b93b0250c29b2aded545178993de1a0f9e5
seungpaleeee <xorwlsdl12@naver.com>
2023-12-27 13:06:13 +0900

    두본째 메뉴별 그림

---
+++ Script/s1.js
@@ -0,0 +1,20 @@
+$(function(){
+
+    $(".main > li, .img_area").mouseenter(function(){
+        $(".sub, .img_area").stop().show();
+    })//mouseenter
+
+    $(".main > li, .img_area").mouseleave(function(){
+        $(".sub, .img_area").stop().hide();
+    })//mouseleave
+
+    $(".main > li").mouseenter(function(){
+        var n = $(this).index();
+        console.log("n :", n)
+
+        $(".img_area .icons li").hide();
+        $(".img_area .icons li").eq(n).show();
+        
+    })
+
+})//jq
